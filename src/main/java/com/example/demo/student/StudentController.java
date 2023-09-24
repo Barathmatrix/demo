ec7946163538ffa417d80db3f902ef101a993e75
barathkumar <barathkumar.raja@paytabs.com>
2023-09-24 16:27:32 +0530

    demo app

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    private final StudentService StudentService;
+    @Autowired
+    public StudentController(StudentService StudentService){
+        this.StudentService = StudentService;
+    }
+
+  	@GetMapping
+	public Student getStudent(){
+		return StudentService.getStudent();
+	}  
+}
